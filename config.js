b63d9b7d770cf17f75c2c4cb81a84ea8145d69d8
maxxijewsa <gnsnoob76@gmail.com>
2025-05-18 02:08:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["1291010846264135800", ""], 
   mongodbUri : "mongodb+srv://sestro:sestro@cluster0.zldd64l.mongodb.net/?retryWrites=true&w=majority",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  spotifyClientId : "56a295f2e2d0424588992d92275cd1c8",
+  spotifyClientSecret : "eba3e96a03334f7a832ae5f06ceb48d2",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
