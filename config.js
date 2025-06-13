d64290ca562a155041577f20b2cd52b287e71415
maxxijewsa <gnsnoob76@gmail.com>
2025-06-14 00:04:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1291010846264135800", ""], 
+  ownerID: ["1378290223909437552", ""], 
   mongodbUri : "mongodb+srv://sestro:sestro@cluster0.zldd64l.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "56a295f2e2d0424588992d92275cd1c8",
   spotifyClientSecret : "eba3e96a03334f7a832ae5f06ceb48d2",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "Music", 
+  activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
@@ -19,8 +19,8 @@ module.exports = {
      {
       name: "GlaceYT",
       password: "glaceyt",
-      host: "193.226.78.187",
-      port:  3543,
+      host: "5.39.63.207",
+      port:  8262,
       secure: false
     }
   ]
